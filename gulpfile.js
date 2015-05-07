3e619ba95663136155ac20809f5575f31e6bd94e
no-glue <nikolapav1985@gmail.com>
2015-05-07 10:27:07 +0200

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,11 @@ var gulp = require("gulp");
 gulp.task("default", function() {
   // do something default
 });
+
+gulp.task("dev", function() {
+  // compile js files for dev
+});
+
+gulp.task("deploy", function() {
+  // compile js files for deploy
+});
