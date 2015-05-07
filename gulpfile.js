3ff4a28c662f87324e24099d671df7395848c255
no-glue <nikolapav1985@gmail.com>
2015-05-07 11:17:04 +0200

    watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require("gulp"),
-  concat = require("gulp-concat");
+  concat = require("gulp-concat"),
+  watch = require("gulp-watch");
 
 gulp.task("default", function() {
   // do something default
@@ -7,9 +8,27 @@ gulp.task("default", function() {
 
 gulp.task("dev", function() {
   // compile js files for dev
-  return gulp.src(["./public/javascripts/app.js", "./public/javascripts/services/*.js","./public/javascripts/controllers/*.js"])
+  return gulp.src([
+      "./public/javascripts/app.js", 
+      "./public/javascripts/services/*.js", 
+      "./public/javascripts/controllers/*.js"
+    ])
+    .pipe(watch([
+      "./public/javascripts/app.js", 
+      "./public/javascripts/services/*.js", 
+      "./public/javascripts/controllers/*.js"
+     ]))
     .pipe(concat("app.js"))
     .pipe(gulp.dest("./public/javascripts/dev"));
+});
+
+gulp.task("watch", function() {
+  // watch for changes in js files
+  return gulp.watch([
+      "./public/javascripts/app.js", 
+      "./public/javascripts/services/*.js", 
+      "./public/javascripts/controllers/*.js"
+    ], ["dev"]);
 });
 
 gulp.task("deploy", function() {
