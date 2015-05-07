f5622c4570ed3fa67653309ecf1ac95ac8a48958
no-glue <nikolapav1985@gmail.com>
2015-05-07 10:47:05 +0200

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
-var gulp = require("gulp");
+var gulp = require("gulp"),
+  concat = require("gulp-concat");
 
 gulp.task("default", function() {
   // do something default
@@ -6,6 +7,9 @@ gulp.task("default", function() {
 
 gulp.task("dev", function() {
   // compile js files for dev
+  return gulp.src(["./public/javascripts/app.js", "./public/javascripts/controllers/*.js"])
+    .pipe(concat("app.js"))
+    .pipe(gulp.dest("./public/javascripts/dev"));
 });
 
 gulp.task("deploy", function() {
